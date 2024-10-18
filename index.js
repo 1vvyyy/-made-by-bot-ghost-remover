67f36a3e8bd12267cac661c035d77d0a69e6f0a7
1vvyyy <zerotwoiswaifu100@gmail.com>
2024-10-17 18:55:27 -0700

    Update index.js
    Changed the desc to remove the "in a ticket"

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Sending Followers","✅ Type /start in a ticket!"];
+const statusMessages = ["🎧 Sending Followers","✅ Type /start"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
